87fbe8c145c895017be31a31dc4ac93507cdb1eb
awimberly <awimberly@comptia.global>
2025-06-07 18:56:00 -0500

    feature: pass data to financial chart when a row is selected

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 // React
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // Styles & Demo Utils
-import Header from './layout/Header';
-import { getData } from './utils/data';
-import './App.css';
+import Header from "./layout/Header";
+import { getData } from "./utils/data";
+import "./App.css";
 
 // AG Grid Module Registration
-import { AgChartsEnterpriseModule } from 'ag-charts-enterprise';
-import { ModuleRegistry } from 'ag-grid-community';
-import { AllEnterpriseModule } from 'ag-grid-enterprise';
+import { AgChartsEnterpriseModule } from "ag-charts-enterprise";
+import { ModuleRegistry } from "ag-grid-community";
+import { AllEnterpriseModule } from "ag-grid-enterprise";
 
 // Custom Components
-import DataGrid from './components/DataGrid';
-import FinancialChart from './components/FinancialChart';
+import DataGrid from "./components/DataGrid";
+import FinancialChart from "./components/FinancialChart";
 
 // AG Grid Module Registration (move outside component)
 ModuleRegistry.registerModules([
@@ -22,6 +22,8 @@ ModuleRegistry.registerModules([
 
 function App() {
   const [selectedRow, setSelectedRow] = useState<any>(null);
+
+  console.log(`Selected Row: ${selectedRow}`);
 
   return (
     <div className="container">
